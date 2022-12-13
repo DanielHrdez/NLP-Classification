a38f422b8f372e74c5a8745fbebe2501b7b09532
Daniel Hernández de León <alu0101331720@ull.edu.es>
2022-12-13 17:39:07 +0000

    da3

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -16,7 +16,7 @@ import emoji
 from symspellpy import SymSpell
 import pkg_resources
 from nltk.stem import PorterStemmer, WordNetLemmatizer
-from stop_words import STOP_WORDS
+from constants import PUNTUATION_MARKS, STOP_WORDS
 
 def parse_arguments(argument_list: list[str]) -> dict:
     """
@@ -52,121 +52,131 @@ def ask_parameters() -> dict:
     parameters['stemming'] = input('Stemming? (y/n): ').lower()
     if parameters['stemming'] != 'y':
         parameters['lemmatization'] = input('Lemmatization? (y/n): ').lower()
+    else:
+        parameters['lemmatization'] = 'n'
     return parameters
 
-def lowercase(text: str, option: str) -> str:
+def lowercase(tokens: set[str], option: str) -> set[str]:
     """
-    Lowercase the text
-        :param text: text to lowercase
+    Lowercase the tokens
+        :param tokens: set to lowercase
         :param lowercase: lowercase? (y/n)
-        :return: text lowercased
+        :return: set lowercased
     """
-    return text.lower() if option == 'y' else text
+    return {token.lower() for token in tokens if token} if option == 'y' else tokens
 
-def puntuation_marks(text: str, option: str) -> str:
+def puntuation_marks(tokens: set[str], option: str) -> set[str]:
     """
     Remove punctuation marks
-        :param text: text to remove punctuation marks
+        :param tokens: set to remove punctuation marks
         :param punctuation_marks: remove punctuation marks? (y/n)
-        :return: text without punctuation marks
-    """
-    return re.sub(r'[:.,;!?]', '', text) if option == 'y' else text
-
-def stopwords(tokens: list[str], option: str) -> list[str]:
-    """
-    Remove stopwords
-        :param tokens: list of tokens
-        :param stopwords: remove stopwords? (y/n)
-        :return: list of tokens without stopwords
+        :return: set without punctuation marks
     """
     if option == 'y':
-        return [token for token in tokens if token not in STOP_WORDS]
+        return {re.sub(rf"[{'|'.join(PUNTUATION_MARKS)}]", '', token) for token in tokens if token}
     return tokens
 
-def emojis(text: str, option: str) -> str:
+def stopwords(tokens: set[str], option: str) -> set[str]:
+    """
+    Remove stopwords
+        :param tokens: set of tokens
+        :param stopwords: remove stopwords? (y/n)
+        :return: set of tokens without stopwords
+    """
+    return {token for token in tokens if token not in STOP_WORDS} if option == 'y' else tokens
+
+def emojis(tokens: set[str], option: str) -> set[str]:
     """
     Remove emojis
-        :param text: text to remove emojis
+        :param tokens: set to remove emojis
         :param emojis: remove emojis? (y/n/w)
-        :return: text without emojis
+        :return: set without emojis
     """
     if option == 'y':
-        return emoji.replace_emoji(text, '')
+        return {emoji.replace_emoji(token, '') for token in tokens if token}
     if option == 'w':
-        return emoji.demojize(text)
-    return text
+        return {emoji.demojize(token) for token in tokens if token}
+    return tokens
 
-def url_html_hashtags(text: str, option: str) -> str:
+def url_html_hashtags(tokens: set[str], option: str) -> set[str]:
     """
     Remove URLs and HTML hashtags
-        :param text: text to remove URLs and HTML hashtags
+        :param tokens: set to remove URLs and HTML hashtags
         :param url_html_hashtags: remove URLs and HTML hashtags? (y/n)
-        :return: text without URLs and HTML hashtags
+        :return: set without URLs and HTML hashtags
     """
     if option == 'y':
-        return re.sub(r'http\S+|#\S+|<.*?>', '', text)
-    return text
+        return {re.sub(r'http.*|#.*|<.*>|@.*', '', token) for token in tokens if token}
+    return tokens
 
-def spell_check(text: str, option: str) -> str:
+def spell_check(tokens: set[str], option: str) -> set[str]:
     """
-    Spell check the text
-        :param text: text to spell check
+    Spell check the tokens
+        :param tokens: set to spell check
         :param spell_check: spell check? (y/n)
-        :return: text without spelling errors
+        :return: set without spelling errors
     """
     if option == 'y':
         sym_spell = SymSpell(max_dictionary_edit_distance=1, prefix_length=7)
         dictionary_path = pkg_resources.resource_filename("symspellpy",
-                                                          "frequency_dictionary_en_82_765.txt")
+                                                            "frequency_dictionary_en_82_765.txt")
         bigram_path = pkg_resources.resource_filename("symspellpy",
-                                                      "frequency_bigramdictionary_en_243_342.txt")
+                                                        "frequency_bigramdictionary_en_243_342.txt")
         sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
         sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)
-        return sym_spell.lookup_compound(text,
-                                         max_edit_distance=1,
-                                         ignore_non_words=True,
-                                         split_by_space=True)
-    return text
+        result = set()
+        for token in tokens:
+            if token:
+                suggestions = sym_spell.lookup_compound(token, max_edit_distance=1)
+                if suggestions:
+                    term = suggestions[0].term
+                    splitted = term.split(' ')
+                    if len(splitted) > 1:
+                        result.add(splitted[0])
+                        result.add(splitted[1])
+                    else:
+                        result.add(term)
+        return result
+    return tokens
 
-def stemming(tokens: list[str], option: str) -> list[str]:
+def stemming(tokens: set[str], option: str) -> set[str]:
     """
-    Stemming the text
-        :param tokens: list of tokens
+    Stemming the tokens
+        :param tokens: set of tokens
         :param stemming: stemming? (y/n)
-        :return: list of tokens with stemming
+        :return: set of tokens with stemming
     """
     if option == 'y':
         stemmer = PorterStemmer()
-        return [stemmer.stem(word) for word in tokens]
+        return {stemmer.stem(word) for word in tokens if word}
     return tokens
 
-def lemmatization(tokens: list[str], option: str) -> list[str]:
+def lemmatization(tokens: set[str], option: str) -> set[str]:
     """
-    Lemmatization the text
-        :param tokens: list of tokens
+    Lemmatization the tokens
+        :param tokens: set of tokens
         :param lemmatization: lemmatization? (y/n)
-        :return: list of tokens with lemmatization
+        :return: set of tokens with lemmatization
     """
     if option == 'y':
         lemmatizer = WordNetLemmatizer()
-        return [lemmatizer.lemmatize(word, pos='v') for word in tokens]
+        return {lemmatizer.lemmatize(word, pos='v') for word in tokens if word}
     return tokens
 
-def tokenize(text: str, parameters: dict) -> list[str]:
+def tokenize(text: list[str], parameters: dict) -> set[str]:
     """
     Tokenize the text
         :param text: text to tokenize
         :param parameters: dictionary with the parameters
-        :return: list of tokens
+        :return: set of tokens
     """
-    text = re.sub(r'\n', ' ', text)
-    text = lowercase(text, parameters['lowercase'])
-    text = puntuation_marks(text, parameters['punctuation_marks'])
-    text = emojis(text, parameters['emojis'])
-    text = url_html_hashtags(text, parameters['url_html_hashtags'])
-    text = spell_check(text, parameters['spell_check'])
-    tokens = text.split()
+    tokens = set(text.split())
+    tokens = lowercase(tokens, parameters['lowercase'])
+    tokens = puntuation_marks(tokens, parameters['punctuation_marks'])
     tokens = stopwords(tokens, parameters['stopwords'])
+    tokens = emojis(tokens, parameters['emojis'])
+    tokens = url_html_hashtags(tokens, parameters['url_html_hashtags'])
+    tokens = spell_check(tokens, parameters['spell_check'])
     tokens = stemming(tokens, parameters['stemming'])
     return lemmatization(tokens, parameters['lemmatization'])
 
@@ -193,8 +203,8 @@ def main() -> None:
     input_filename, output_filename = parse_arguments(sys.argv[1:])
     parameters = ask_parameters()
     data_frame = pandas.read_excel(input_filename)
-    list_all_text = data_frame.iloc[:, 0].str.cat(sep=' ')
-    tokens = tokenize(list_all_text, parameters)
+    all_text = data_frame.iloc[:, 0].str.cat(sep=' ')
+    tokens = tokenize(all_text, parameters)
     write_file(output_filename, tokens)
 
 if __name__ == '__main__':
